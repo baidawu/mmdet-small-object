818ec1ce5fb77c9afdbe001f68cf5b5774c69850
zgl <huangxiaowen0804@163.com>
2022-06-26 19:08:48 +0800

    first success!

--- myConf/my_cascade.py
+++ myConf/my_cascade.py
@@ -360,7 +360,7 @@ runner = dict(type='EpochBasedRunner', max_epochs=12)
 # TODO 
 checkpoint_config = dict(interval=1)
 
-
+#  
 log_config = dict(interval=50, hooks=[dict(type='TextLoggerHook')])
 custom_hooks = [dict(type='NumClassCheckHook')]
 dist_params = dict(backend='nccl')
